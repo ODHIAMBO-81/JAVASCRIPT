1d0641e2ac73c1b2b4156bb59f6d561358109b79
ODHIAMBO-81 <reaganodhiambo36@gmail.com>
2025-09-21 22:10:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function addTask() {
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
+
+  if (taskText === "") return;
+
+  const li = document.createElement('li');
+  li.textContent = taskText;
+    // Mark task as completed on click
+  li.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  // Create delete button
+  const removeBtn = document.createElement("button");
+  removeBtn.textContent = "Delete";
+  removeBtn.onclick = () => li.remove();
+
+  li.appendChild(removeBtn);
+  document.getElementById("taskList").appendChild(li);
+
+  taskInput.value = ""; // Clear Input field after adding
+}
+
+  
+
+
+
+
+
+
