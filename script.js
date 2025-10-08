5b9ae56049c745bb057fc46caf69463ad54a56cd
ODHIAMBO-81 <reaganodhiambo36@gmail.com>
2025-10-08 15:39:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
-function addTask() {
-  const taskInput = document.getElementById('taskInput');
-  const taskText = taskInput.value.trim();
+async function getWeather() {
+  let city = document.getElementById("city").value;
+  let apiKey = "a16984955882f66f42ce62e172f8fadb";// 🔑 Replace with your OpenWeatherMap API key
 
-  if (taskText === "") return;
+  let url =" https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric";
 
-  const li = document.createElement('li');
-  li.textContent = taskText;
-    // Mark task as completed on click
-  li.addEventListener("click", () => {
-    li.classList.toggle("completed");
-  });
+  try {
+    let response = await fetch(url);
+    if (!response.ok) throw new Error("City not found!");
 
-  // Create delete button
-  const removeBtn = document.createElement("button");
-  removeBtn.textContent = "Delete";
-  removeBtn.onclick = () => li.remove();
-
-  li.appendChild(removeBtn);
-  document.getElementById("taskList").appendChild(li);
-
-  taskInput.value = ""; // Clear Input field after adding
+    let data = await response.json();
+    document.getElementById("weather").innerHTML = `ppppppppppppppppppppppppppppppppppppp
+      <h3>${data.name}, ${data.sys.country}</h3>
+      <p>🌡 Temperature: ${data.main.temp} °C</p>
+      <p>☁ Condition: ${data.weather[0].description}</p>
+      <p>💧 Humidity: ${data.main.humidity}%</p>
+      <p>🌬 Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+  } catch (error) {
+    document.getElementById("weather").innerHTML = <p style="color:red;">${error.message}</p>;
+  }
 }
-
-  
-
-
-
-
-
-
